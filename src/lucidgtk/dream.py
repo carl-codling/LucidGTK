6e3ba0f3ecbb510ada69148b50abbe8d46279c8e
carl-codling <carlos@freefalldigital.co.uk>
2015-08-07 18:03:30 +0100

    Output img sequence as .avi + interface reshuffle

--- src/lucidgtk/dream.py
+++ src/lucidgtk/dream.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk, Gio, GLib
+from gi.repository import Gtk, Gio, GLib, Gdk
 
 from cStringIO import StringIO
 import numpy as np
@@ -32,6 +32,7 @@ from os.path import basename
 from gi.repository.GdkPixbuf import Pixbuf
 
 import json
+import cv2
 
 from lucidgtk.settingsWin import SettingsWindow
 from lucidgtk.VideoWindow import VideoWindow
@@ -48,13 +49,13 @@ class DreamWindow(Gtk.Window):
         self.settings = Gio.Settings('org.rebelweb.dreamer')
         Gtk.Window.__init__(self, title='Lucid-GTK')
         self.set_icon_name('lucid-gtk')
-        self.iconify()
         
         #self.set_title("%s v%s" % (self.package, self.version))
         self.set_border_width(10)
         self.grid = Gtk.Grid()
         self.add(self.grid)
         
+        self.fps = False # if not set then output videos default to settings obj. value
         
         if self.initcaffe() is False:
             self.do_config_error('Caffe could not start. Please review the model and deploy file settings')
@@ -68,10 +69,13 @@ class DreamWindow(Gtk.Window):
         
         
         self.do_top_bar()
+        self.do_adjustments_bar()
         self.do_info_bar()
         self.set_image('.temp/temp.jpg')
         self.do_bottom_bar()
         self.do_notif_bar()
+        
+        self.show_all()
     
     def media_folders_set(self):
         if os.path.isdir(self.settings.get_string('im-dir')) and os.path.isdir(self.settings.get_string('vid-dir')):
@@ -123,15 +127,10 @@ class DreamWindow(Gtk.Window):
     
     def enable_buttons(self, v=True):
         self.settingsBtn.set_sensitive(v)
-        self.fileBtn.set_sensitive(v)
-        self.vidBtn.set_sensitive(v)
+        self.fBtn.set_sensitive(v)
         self.dreamBtn.set_sensitive(v)
-        self.saveBtn.set_sensitive(v)
-        self.iterSpin.set_sensitive(v)
-        self.octaveSpin.set_sensitive(v)
-        self.scaleSpin.set_sensitive(v)
-        self.loopSpin.set_sensitive(v)
-        self.layer_combo.set_sensitive(v)
+        self.inpCombo.set_sensitive(v)
+        self.outpCombo.set_sensitive(v)
     
     def get_shrink_dimensions(self, w, h, bytesize, limit, ch=3):
         while bytesize > limit:
@@ -223,14 +222,8 @@ class DreamWindow(Gtk.Window):
                     self.set_info("Frame: "+str(self.loop+1)+"/"+str(int(self.vidWin.cap.get(7)))+" | Octave: "+str(octave+1)+" | Iter: "+str(i+1))
                 while Gtk.events_pending():
                 	Gtk.main_iteration_do(True)
-            # extract details produced on the current octave
             detail = src.data[0]-octave_base
             
-        # returning the resulting image
-        if self.autoSaveBtn.get_active():
-        	self.save_image()
-        self.set_info("")
-        self.set_notif('<span foreground="blue">Ready to dream. Counting electric sheep</span>')
         return self.deprocess(net, src.data[0])
     
     # a couple of utility functions for converting to and from Caffe's input image layout
@@ -259,7 +252,7 @@ class DreamWindow(Gtk.Window):
         self.infoBar = Gtk.Box(spacing=10)
         self.infoLabel = Gtk.Label("")
         self.infoBar.add(self.infoLabel)
-        self.grid.attach_next_to(self.infoBar, self.topBar, Gtk.PositionType.BOTTOM, 1, 3)
+        self.grid.attach_next_to(self.infoBar, self.adjBar, Gtk.PositionType.BOTTOM, 1, 3)
     
     def set_info(self, msg):
     	self.infoLabel.set_markup('<span color="green">'+msg+'</span>')
@@ -268,11 +261,82 @@ class DreamWindow(Gtk.Window):
         SettingsWindow(self)
     
     
-    def on_vidbtn_clicked(self,btn):
+    def on_vidbtn_clicked(self):
         self.vidWin = VideoWindow(self)
     
     def set_notif(self, msg):
     	self.notif.set_markup('<span size="larger">'+msg+'</span>')
+    
+    def do_adjustments_bar(self):
+        self.adjBar = Gtk.Box()
+        
+        nloops = self.settings.get_int('n-loops')
+    	label = Gtk.Label("Loops:")
+        self.adjBar.add(label)
+        adjustment = Gtk.Adjustment(nloops, 1, 99999, 1, 0, 0)
+        self.loopSpin = Gtk.SpinButton()
+        self.loopSpin.set_adjustment(adjustment)
+        self.loopSpin.set_value(nloops)
+        self.loopSpin.set_numeric(1)
+        self.loopSpin.connect("value-changed", self.set_loops_val)
+        self.adjBar.pack_start(self.loopSpin, False, False, 0)
+    	
+        iter_val = self.settings.get_int('n-iterations')
+        label = Gtk.Label("Iterations:")
+        self.adjBar.add(label)
+        adjustment = Gtk.Adjustment(iter_val, 1, 37, 1, 10, 0)
+        self.iterSpin = Gtk.SpinButton()
+        self.iterSpin.set_adjustment(adjustment)
+        self.iterSpin.set_value(iter_val)
+        self.iterSpin.set_numeric(1)
+        self.iterSpin.connect("value-changed", self.set_iter_val)
+        self.adjBar.pack_start(self.iterSpin, False, False, 0)
+        
+        octv_val = self.settings.get_int('n-octaves')
+        label = Gtk.Label("Octaves:")
+        self.adjBar.add(label)
+        adjustment = Gtk.Adjustment(octv_val, 1, 23, 1, 0, 0)
+        self.octaveSpin = Gtk.SpinButton()
+        self.octaveSpin.set_adjustment(adjustment)
+        self.octaveSpin.set_value(octv_val)
+        self.octaveSpin.set_numeric(1)
+        self.octaveSpin.connect("value-changed", self.set_octv_val)
+        self.adjBar.pack_start(self.octaveSpin, False, False, 0)
+        
+        octv_scale = self.settings.get_double('octave-scale')
+        label = Gtk.Label("Scale:")
+        self.adjBar.add(label)
+        adjustment = Gtk.Adjustment(octv_scale, 1.00, 1.90, 0.1, 0, 0)
+        self.scaleSpin = Gtk.SpinButton()
+        self.scaleSpin.configure(adjustment,0.01,2)
+        self.scaleSpin.set_value(octv_scale)
+        self.scaleSpin.set_numeric(1)
+        self.scaleSpin.connect("value-changed", self.set_scale_val)
+        self.adjBar.pack_start(self.scaleSpin, False, False, 0)
+        
+        zoom_scale = self.settings.get_double('zoom-scale')
+        label = Gtk.Label("Zoom:")
+        self.adjBar.add(label)
+        adjustment = Gtk.Adjustment(zoom_scale, 0.00, 0.10, 0.01, 0, 0)
+        self.zoomSpin = Gtk.SpinButton()
+        self.zoomSpin.configure(adjustment,0.01,2)
+        self.zoomSpin.set_value(zoom_scale)
+        self.zoomSpin.set_numeric(1)
+        self.zoomSpin.connect("value-changed", self.set_zoom_val)
+        self.adjBar.pack_start(self.zoomSpin, False, False, 0)
+        
+        deg_val = self.settings.get_double('rot-deg')
+        label = Gtk.Label("Rotation:")
+        self.adjBar.add(label)
+        adjustment = Gtk.Adjustment(deg_val, -10.00, 10.00, 0.10, 0, 0)
+        self.degSpin = Gtk.SpinButton()
+        self.degSpin.configure(adjustment,0.10,2)
+        self.degSpin.set_value(deg_val)
+        self.degSpin.set_numeric(1)
+        self.degSpin.connect("value-changed", self.set_deg_val)
+        self.adjBar.pack_start(self.degSpin, False, False, 0)
+        
+        self.grid.attach_next_to(self.adjBar, self.topBar, Gtk.PositionType.BOTTOM, 1, 3)
     
     def do_top_bar(self):
     	self.topBar = Gtk.Box()
@@ -281,76 +345,34 @@ class DreamWindow(Gtk.Window):
         #logo.set_from_pixbuf(pb)
         #self.topBar.pack_start(logo, False, False, 0)
         
-        iter_val = self.settings.get_int('n-iterations')
-        label = Gtk.Label("Iterations:")
-        self.topBar.add(label)
-        adjustment = Gtk.Adjustment(iter_val, 1, 37, 1, 10, 0)
-        self.iterSpin = Gtk.SpinButton()
-        self.iterSpin.set_adjustment(adjustment)
-        self.iterSpin.set_value(iter_val)
-        self.iterSpin.set_numeric(1)
-        self.iterSpin.connect("value-changed", self.set_iter_val)
-        self.topBar.pack_start(self.iterSpin, False, False, 0)
-        
-        octv_val = self.settings.get_int('n-octaves')
-        label = Gtk.Label("Octaves:")
-        self.topBar.add(label)
-        adjustment = Gtk.Adjustment(octv_val, 1, 23, 1, 0, 0)
-        self.octaveSpin = Gtk.SpinButton()
-        self.octaveSpin.set_adjustment(adjustment)
-        self.octaveSpin.set_value(octv_val)
-        self.octaveSpin.set_numeric(1)
-        self.octaveSpin.connect("value-changed", self.set_octv_val)
-        self.topBar.pack_start(self.octaveSpin, False, False, 0)
-        
-        octv_scale = self.settings.get_double('octave-scale')
-        label = Gtk.Label("Scale:")
-        self.topBar.add(label)
-        adjustment = Gtk.Adjustment(octv_scale, 1.00, 1.90, 0.1, 0, 0)
-        self.scaleSpin = Gtk.SpinButton()
-        self.scaleSpin.configure(adjustment,0.01,2)
-        self.scaleSpin.set_value(octv_scale)
-        self.scaleSpin.set_numeric(1)
-        self.scaleSpin.connect("value-changed", self.set_scale_val)
-        self.topBar.pack_start(self.scaleSpin, False, False, 0)
-        
-        zoom_scale = self.settings.get_double('zoom-scale')
-        label = Gtk.Label("Zoom:")
-        self.topBar.add(label)
-        adjustment = Gtk.Adjustment(zoom_scale, 0.00, 0.10, 0.01, 0, 0)
-        self.zoomSpin = Gtk.SpinButton()
-        self.zoomSpin.configure(adjustment,0.01,2)
-        self.zoomSpin.set_value(zoom_scale)
-        self.zoomSpin.set_numeric(1)
-        self.zoomSpin.connect("value-changed", self.set_zoom_val)
-        self.topBar.pack_start(self.zoomSpin, False, False, 0)
-        
-        deg_val = self.settings.get_double('rot-deg')
-        label = Gtk.Label("Rotation:")
-        self.topBar.add(label)
-        adjustment = Gtk.Adjustment(deg_val, -10.00, 10.00, 0.10, 0, 0)
-        self.degSpin = Gtk.SpinButton()
-        self.degSpin.configure(adjustment,0.10,2)
-        self.degSpin.set_value(deg_val)
-        self.degSpin.set_numeric(1)
-        self.degSpin.connect("value-changed", self.set_deg_val)
-        self.topBar.pack_start(self.degSpin, False, False, 0)
-        
-        label = Gtk.Label("Layer:")
+    	
+        label = Gtk.Label("Output Layer:")
         self.topBar.add(label)
         self.topBar.pack_start(self.make_layer_select(), False, False, True)
-
-        self.fileBtn = Gtk.Button("PHOTO")
-        self.fileBtn.connect("clicked", self.on_fselect_clicked)
-        self.topBar.pack_start(self.fileBtn, False, False, 0)
         
-        self.vidBtn = Gtk.Button("VIDEO")
-        self.vidBtn.connect("clicked", self.on_vidbtn_clicked)
-        self.topBar.pack_start(self.vidBtn, False, False, 0)
+        label = Gtk.Label("Input:")
+        self.topBar.add(label)
+        inp_store = Gtk.ListStore(int, str)
+        inp_store.append([1, "Image"])
+        inp_store.append([2, "Video"])
+        self.inpCombo = Gtk.ComboBox.new_with_model(inp_store)
+        #inp_combo.connect("changed", self.on_inp_combo_changed)
+        renderer_text = Gtk.CellRendererText()
+        self.inpCombo.pack_start(renderer_text, True)
+        self.inpCombo.add_attribute(renderer_text, "text", 1)
+        self.inpCombo.set_active(0)
+        self.topBar.pack_start(self.inpCombo, False, False, True)
         
-        self.dreamBtn = Gtk.Button('DREAM')
+        
+        self.fBtn = Gtk.Button("Select")
+        self.fBtn.connect("clicked", self.on_fBtn_clicked)
+        self.topBar.pack_start(self.fBtn, False, False, 0)
+        
+        self.dreamBtn = Gtk.Button('START DREAMING')
+        
         self.dreamBtn.connect("clicked", self.on_dream_clicked)
         self.topBar.pack_start(self.dreamBtn, False, False, 0)
+        self.topBar.set_child_packing(self.dreamBtn, False, True, 0, 1)
         
         self.grid.attach(self.topBar,1,1,2,1)
         
@@ -379,32 +401,26 @@ class DreamWindow(Gtk.Window):
     	self.bottBar = Gtk.Box()
     	self.grid.attach_next_to(self.bottBar, self.scrollWin, Gtk.PositionType.BOTTOM, 1, 3)
     	
-    	nloops = self.settings.get_int('n-loops')
-    	label = Gtk.Label("Loops:")
+    	label = Gtk.Label("Output as:")
         self.bottBar.add(label)
-        adjustment = Gtk.Adjustment(nloops, 1, 99999, 1, 0, 0)
-        self.loopSpin = Gtk.SpinButton()
-        self.loopSpin.set_adjustment(adjustment)
-        self.loopSpin.set_value(nloops)
-        self.loopSpin.set_numeric(1)
-        self.loopSpin.connect("value-changed", self.set_loops_val)
-        self.bottBar.pack_start(self.loopSpin, False, False, 0)
-    	
-    	label = Gtk.Label("Auto Save:")
-        self.bottBar.add(label)
-        self.autoSaveBtn = Gtk.CheckButton()
-        self.autoSaveBtn.set_active(self.settings.get_boolean('auto-save'))
-        self.autoSaveBtn.connect("toggled", self.set_autosave)
-        self.bottBar.pack_start(self.autoSaveBtn, False, False, 0)
         
         self.imageName = Gtk.Entry()
         self.imageName.set_text("myImageName")
         self.bottBar.pack_start(self.imageName, True, True, 0)
         
-    	self.saveBtn = Gtk.Button("SAVE")
-        self.saveBtn.connect("clicked", self.save_image)
-        self.bottBar.pack_start(self.saveBtn, False, False, 0)
-    
+        outp_store = Gtk.ListStore(int, str)
+        outp_store.append([0, "DON'T SAVE"])
+        outp_store.append([1, ".JPG"])
+        outp_store.append([2, ".AVI"])
+        outp_store.append([3, ".JPG & .AVI"])
+        self.outpCombo = Gtk.ComboBox.new_with_model(outp_store)
+        #inp_combo.connect("changed", self.on_inp_combo_changed)
+        renderer_text = Gtk.CellRendererText()
+        self.outpCombo.pack_start(renderer_text, True)
+        self.outpCombo.add_attribute(renderer_text, "text", 1)
+        self.outpCombo.set_active(0)
+        self.bottBar.pack_start(self.outpCombo, False, False, True)
+        
     	
     def set_image(self, im):
         self.scrollWin = Gtk.ScrolledWindow()
@@ -436,23 +452,46 @@ class DreamWindow(Gtk.Window):
     	image.save(fp, optimize=True)
     	self.set_notif('<span foreground="black" background="yellow">Current dream state saved to <span foreground="blue">'+fp+'</span></span>')
     
-    def make_new_fname(self):
-        fp = self.settings.get_string('im-dir')+'/'+self.imageName.get_text()+'.jpg'
+    def make_new_fname(self, dirSetting='im-dir', extension='.jpg'):
+        fp = self.settings.get_string(dirSetting)+'/'+self.imageName.get_text()+extension
         if os.path.isfile(fp):
             i = 0
             while True:
-                fp = self.settings.get_string('im-dir')+'/'+self.imageName.get_text()+'_'+str(i)+'.jpg'
+                fp = self.settings.get_string(dirSetting)+'/'+self.imageName.get_text()+'_'+str(i)+extension
                 if os.path.isfile(fp)==False:
                     break
                 i += 1
         return fp
     
+    def init_outp_video(self):
+        fourcc = cv2.cv.CV_FOURCC(*'XVID')
+        if self.fps == False:
+            fps = self.settings.get_int('fps')
+        else:
+            fps = self.fps
+        img = self.prepare_image()
+        h,w = img.shape[:2]    
+        return cv2.VideoWriter(self.make_new_fname(dirSetting='vid-dir', extension='.avi'),fourcc, fps, (w,h))
+    
     def on_dream_clicked(self, button):
         self.mode = 'image'
         self.enable_buttons(False)
+        self.set_notif('<span foreground="white" background="red" weight="heavy">COMPUTER IS DREAMING. DO NOT DISTURB!...</span>')
+            
+        while Gtk.events_pending():
+            Gtk.main_iteration_do(True)
+        
+        tree_iter = self.outpCombo.get_active_iter()
+        if tree_iter != None:
+            model = self.outpCombo.get_model()
+            outpType = model[tree_iter][0]
+            
+        if outpType > 1:
+            vidOut = self.init_outp_video()
+        
     	for i in xrange(int(self.loopSpin.get_value())):
     	    self.loop = i
-            self.set_notif('<span foreground="white" background="red" weight="heavy">COMPUTER IS DREAMING. DO NOT DISTURB!...</span>')
+            
             img = self.prepare_image()
             
             r = self.settings.get_double('rot-deg')
@@ -465,10 +504,38 @@ class DreamWindow(Gtk.Window):
                 img = nd.affine_transform(img, [1-z,1-z,1], [h*z/2,w*z/2,0], order=1)
             
             outp = self.deepdream(self.net, img)
+            
+            if outpType > 1:
+                imgout = self.prepare_image()
+                imgout = np.uint8(np.clip(imgout, 0, 255))
+                vidOut.write(imgout)
+                
+            if outpType == 1 or outpType == 3:
+        	    self.save_image()
+
+        self.set_info("")
+        self.set_notif('<span foreground="blue">Ready to dream. Counting electric sheep</span>') 
+        
+        if outpType > 1:
+            vidOut.release()
+            cv2.destroyAllWindows()   
+                
         self.enable_buttons()
 
+    def on_fBtn_clicked(self, combo):
+        tree_iter = self.inpCombo.get_active_iter()
+        if tree_iter != None:
+            model = self.inpCombo.get_model()
+            t = model[tree_iter][0]
+            if t is 1:
+                self.on_fselect_clicked()
+            elif t is 2:
+                self.on_vidbtn_clicked()
+            
+        
     
-    def on_fselect_clicked(self, widget):
+    
+    def on_fselect_clicked(self):
         dialog = Gtk.FileChooserDialog("Please choose a file", self,
             Gtk.FileChooserAction.OPEN,
             (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
